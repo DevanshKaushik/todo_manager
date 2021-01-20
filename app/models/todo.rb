3252cb4ee8f1a30368afb255e2ef960dc7f93167
DevanshKaushik <devanshkaushik0711@gmail.com>
2021-01-20 18:06:29 +0530

    Can get a single todo using id in /todos/id

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ActiveRecord::Base
+  def to_pleasant_string
+    is_comlpeted = completed ? "[X]" : "[ ]"
+    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_comlpeted}"
+  end
 end
