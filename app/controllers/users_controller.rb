785f146222fd2a4d670d4113b4fb3a4dc5cd7c8f
DevanshKaushik <devanshkaushik0711@gmail.com>
2021-01-21 16:51:21 +0530

    Added ability to login using email and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,16 @@ class UsersController < ApplicationController
     response_text = "New user is created with the user_id: #{new_user.id}"
     render plain: response_text
   end
+
+  def login
+    user_email = params[:email]
+    user_password = params[:password]
+
+    user = User.find_by(email: user_email, password: user_password)
+    if user.nil?
+      render plain: false
+    else
+      render plain: true
+    end
+  end
 end
