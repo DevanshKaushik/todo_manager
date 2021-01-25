792eaa0c1f0b48186da2044d54262906be384137
DevanshKaushik <devanshkaushik0711@gmail.com>
2021-01-25 20:33:16 +0530

    Added signin functionality (without cookies)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get "/" => "home#index"
   resources :todos
   resources :users
-  post "users/login", to: "users#login"
-  get "/" => "home#index"
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
 end
