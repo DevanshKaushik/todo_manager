f2ebfa925920d10b41ba4d433456ad5f2981ae78
DevanshKaushik <devanshkaushik0711@gmail.com>
2021-01-25 16:40:57 +0530

    Added associations to users and todos tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_231214) do
+ActiveRecord::Schema.define(version: 2021_01_24_003220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,15 +21,16 @@ ActiveRecord::Schema.define(version: 2021_01_23_231214) do
     t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
-    t.string "last_name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "last_name"
   end
 
 end
