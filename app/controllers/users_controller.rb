f7401f6e88fa1dbff82cedac5395c3b6a921f8e1
DevanshKaushik <devanshkaushik0711@gmail.com>
2021-01-26 20:43:36 +0530

    Validating User details before saving

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    new_user = User.create!(
+    new_user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
     )
 
-    session[:current_user_id] = new_user.id
-    redirect_to "/"
+    if new_user.save
+      session[:current_user_id] = new_user.id
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+    end
+
+    redirect_to new_user_path
   end
 end
